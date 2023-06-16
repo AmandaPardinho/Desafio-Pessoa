a9f400ff5db9b5be3880220b849296ebc9239958
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-16 20:09:16 -0300

    Correção na data de nascimento e teste do método para calcular idade

--- DesafioPessoa/Program.cs
+++ DesafioPessoa/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DesafioPessoa;
 
-Person person = new Person("Amanda", "/01/1989", 1.68);
+Person person = new Person("Amanda", "03/01/1989", 1.68);
 
 Console.WriteLine(person.ToString());
+person.CalculateAge();
 
+Console.ReadKey();
