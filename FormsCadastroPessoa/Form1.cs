66e651b76cdc6cfcf9fa4238adf26a6d1c47add0
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-05 18:00:44 -0300

    Alterações no retorno dos métodos para imprimir e limpar

--- FormsCadastroPessoa/Form1.cs
+++ FormsCadastroPessoa/Form1.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.Design.Serialization;
-using static FormsCadastroPessoa.Person;
 
 namespace FormsCadastroPessoa
 {
@@ -10,39 +9,35 @@ namespace FormsCadastroPessoa
             InitializeComponent();
         }
 
+        //Detecta se tem ou não algum texto
         private void txtNome_TextChanged(object sender, EventArgs e)
         {
             string nome = txtNome.Text;
-            if(nome == null)
+            if (nome == "")
             {
                 MessageBox.Show("Insira um nome");
             }
         }
 
+        //determina o que acontece quando o checkbox painel está ticado
         private void cBoxPainel_CheckedChanged(object sender, EventArgs e)
         {
-            if(cBoxPainel.Checked == true)
+            if (cBoxPainel.Checked == true)
             {
                 cBoxPopUp.Checked = false;
             }
-            else
-            {
-                cBoxPopUp.Checked = true;
-            }
         }
 
+        //determina o que acontece quando o checkbox pop up está ticado
         private void cBoxPopUp_CheckedChanged(object sender, EventArgs e)
         {
             if (cBoxPopUp.Checked == true)
             {
                 cBoxPainel.Checked = false;
             }
-            else
-            {
-                cBoxPainel.Checked = true;
-            }            
         }
 
+        //validação que impede que o usuário digite números ou caracteres especiais
         private void txtNome_KeyPress(object sender, KeyPressEventArgs e)
         {
             int tecla = (int)e.KeyChar;
@@ -54,24 +49,41 @@ namespace FormsCadastroPessoa
             }
         }
 
+        //imprime nome, data de nascimento e altura, conforme o checkbox ticado e impede a impressão dessas informações se o usuário não inserir o nome
         private void btnImprimir_Click(object sender, EventArgs e)
         {
-            string nome, data, altura, resultado;
+            string nome, altura, dataFormatada, resultado;
+            DateTime anoAtual, data, idade;
+
+            data = dateCalendario.SelectionRange.Start.Date;
+            // "d" => um dos parâmetros permitidos pela convenção de cadeias de caracteres de formato de data e hora padrão
+            dataFormatada = data.ToString("d");
+
             nome = txtNome.Text;
-            data = dateCalendario.Text;
             altura = dropDownAltura.Text;
-            resultado = $"{nome}\n{data}\n{altura}";
+
+            resultado = $"Nome: {nome}\nData de nascimento: {dataFormatada}\nAltura: {altura}";
+
             try
             {
                 if (cBoxPainel.Checked)
                 {
-                    lblResposta.Visible = true;
-                    lblResposta.Text = resultado;
+                    if (nome == "")
+                    {
+                        txtNome_TextChanged(sender, e);
+                    }
+                    else
+                    {
+                        lblResposta.Visible = true;
+                        lblResposta.Text = resultado;
+                    }
                 }
                 else if (cBoxPopUp.Checked)
                 {
                     MessageBox.Show(resultado);
                 }
+
+
             }
             catch (Exception ex)
             {
@@ -79,18 +91,29 @@ namespace FormsCadastroPessoa
             }
         }
 
+        //limpa os dados da tela
         private void btnLimpar_Click(object sender, EventArgs e)
-        {        
-            lblResposta.ResetText();
-            txtNome.ResetText();
-            dateCalendario.ResetText();
-            dropDownAltura.ResetText();
+        {
+            //retoma o valor inicial do numericUpDown
+            var valorInicial = "0,00";
+            var valorFinal = dropDownAltura.Value.ToString(valorInicial);
+
+            //retoma a posição do calendário para a data de hoje
+            DateTime diaAtual = DateTime.Today;
+            dateCalendario.SetDate(diaAtual);
+
+            //deixa o checkBox do painel e do pop up sem ticar
             if (cBoxPainel.Checked || cBoxPopUp.Checked)
             {
                 cBoxPainel.Checked = false;
                 cBoxPopUp.Checked = false;
-
             }
+
+            //apaga o nome
+            txtNome.ResetText();
+
+            //apaga as informações impressas se o checkBox do painel tiver sido clicado anteriormente
+            lblResposta.ResetText();
         }
     }
 }
