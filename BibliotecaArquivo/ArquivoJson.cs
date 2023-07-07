60a83dd952a9733c14e4bf150f410b82f0730616
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-07 19:19:09 -0300

    Arquivos e pastas criados e modificados automaticamente

---
+++ BibliotecaArquivo/ArquivoJson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BibliotecaArquivo
+{
+    public class ArquivoJson
+    {
+        public static void CriaJson()
+        {
+            string texto = "";
+            if (texto.Length > 0)
+            {
+                string json = D
+            }
+            
+
+        }
+    }
+}
